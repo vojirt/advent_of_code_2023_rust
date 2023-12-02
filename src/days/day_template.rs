290825a86405dc57c484956846cf270ba4045c25
Tomáš Vojíř <vojirtom@fel.cvut.cz>
2023-12-02 15:22:24 +0100

    qol improvements

--- src/days/day_template.rs
+++ src/days/day_template.rs
@@ -1,18 +1,27 @@
+use std::fs;
+
 pub fn solve() {
-    let p1 = solve_part_1();
-    let p2 = solve_part_2();
+    let input = fs::read_to_string("./inputs/input.txt")
+        .expect("File not found")
+        .lines()
+        .map(|l: &str| l.to_string())
+        .collect::<Vec<String>>();
+
+    let p1 = solve_part_1(input.clone());
+    let p2 = solve_part_2(input);
 
     println!("Part 1 solution = {}", p1); 
     println!("Part 2 solution = {}", p2); 
 }
 
-fn solve_part_2() -> u8 {
+fn solve_part_1(input: Vec<String>) -> u8 {
     0
 }
 
-fn solve_part_1() -> u8 {
+fn solve_part_2(input: Vec<String>) -> u8 {
     0
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -20,8 +29,10 @@ mod tests {
 
     #[test]
     fn simple_case() {
-        let p1 = solve_part_1();
-        let p2 = solve_part_2();
+        let input1 = vec!["".to_string()];
+        let input2 = vec!["".to_string()]; 
+        let p1 = solve_part_1(input1);
+        let p2 = solve_part_2(input2);
         assert_eq!(p1, 0);
         assert_eq!(p2, 0);
     }
